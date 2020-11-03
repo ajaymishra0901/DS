9f9a37e6fe6e5fa26d9d772ef74f516953fd9de8
ajaymishra0901 <54370352+ajaymishra0901@users.noreply.github.com>
2020-11-03 20:22:06 +0530

    Add files via upload

---
+++ Practical_1a.py
@@ -0,0 +1,17 @@
+#Name: AjayMishra
+#Roll No: 4061
+#SYCS
+
+# Implement the following for Array:
+# Write a program to store the elements in 1-D array and provide an option 
+# to perform the operations like searching, sorting, merging, reversing the elements.
+arr1=[49,18,77,12,1,4,54]
+arr2=['hello','world']
+arr1.index(49)
+print(arr1)
+arr1.sort()
+print(arr1)
+arr1.extend(arr2)
+print(arr1)
+arr1.reverse()
+print(arr1)
